d70e55c7abb949900a9b65c4d3f936059f5f70ca
jkwok678 <jkwok678@gmail.com>
2021-03-15 01:07:30 +0000

    Initial commit

---
+++ Assets/Scenes/Controller/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Serializable;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rigidbody;
+    [Serializable] private float maxVelocity = 2;
+    [Serializable] private float rotationSpeed = 2;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxis("Horizontal");
+        Thrust(yAxis);
+        Rotate(transform, xAxis * rotationSpeed);
+    }
+
+    private void Thrust(float power)
+    {
+        Vector2 force = transform.up *power;
+        rigidbody.AddForce(force);
+    }
+
+    private void Rotate(Transform transform, float amount)
+    {
+        transform.Rotate(0,0,amount);
+    }
+}
