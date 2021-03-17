dca918725188072c83001e2d41b55d0bd0711064
jkwok678 <jkwok678@gmail.com>
2021-03-17 10:18:19 +0000

    First commit
    Player can move,
    First asteroid created with movement
    Asteroid prefab made

---
+++ Assets/Controller/DonutController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonutController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    [SerializeField] private float maxForce;
+    [SerializeField] private float topY;
+
+    [SerializeField] private float bottomY;
+
+    [SerializeField] private float rightX;
+
+    [SerializeField] private float leftX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        Vector2 thrust = new Vector2(Random.Range(-maxForce, maxForce), Random.Range(-maxForce, maxForce));
+        rigidbody.AddForce(thrust);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Vector2 newLocation = transform.position;
+        if (transform.position.y > topY)
+        {
+            newLocation.y = bottomY;
+        }
+        if (transform.position.y < bottomY)
+        {
+            newLocation.y = topY;
+        }
+        if (transform.position.x > rightX)
+        {
+            newLocation.x = leftX;
+        }
+        if (transform.position.x < leftX)
+        {
+            newLocation.x = rightX;
+        }
+        transform.position = newLocation;
+    }
+}
---
+++ Assets/Controller/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rigidbody;
+    private float maxVelocity = 2;
+    [SerializeField] private float rotationSpeed;
+    private float thrust;
+    private float turn;
+    [SerializeField] private float topY;
+
+    [SerializeField] private float bottomY;
+
+    [SerializeField] private float rightX;
+
+    [SerializeField] private float leftX;
+    
+    [SerializeField] private GameObject laser;
+    [SerializeField] private float laserspeed;
+    [SerializeField] private float destroyLaserTime;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thrust = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+
+        Vector2 newLocation = transform.position;
+        if (transform.position.y > topY)
+        {
+            newLocation.y = bottomY;
+        }
+        if (transform.position.y < bottomY)
+        {
+            newLocation.y = topY;
+        }
+        if (transform.position.x > rightX)
+        {
+            newLocation.x = leftX;
+        }
+        if (transform.position.x < leftX)
+        {
+            newLocation.x = rightX;
+        }
+        transform.position = newLocation;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newLaser = Instantiate(laser, transform.position, transform.rotation);
+            newLaser.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * laserspeed);
+            Destroy(newLaser, destroyLaserTime);
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        rigidbody.AddRelativeForce(Vector2.up * thrust);
+        rigidbody.AddTorque(-turn * rotationSpeed);
+    }
+    
+}
